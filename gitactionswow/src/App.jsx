a925c3832fec52047ef00fc2732fbd95950f62e3
KVV <2300033045@kluniversity.in>
2025-10-18 02:32:22 +0530

    wow

---
+++ gitactionswow/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Git Actions WOW</h1>
+      <p>wow </p>
+    </>
+  )
+}
+
+export default App
